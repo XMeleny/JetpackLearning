60fd70fbc52253a92bd97c5afd7cd1090e125645
zhuxiaomei <zhuxiaomei.meleny@bytedance.com>
2020-07-03 15:02:45 +0800

    数据绑定

--- app/src/main/java/com/xm/jetpacklearning/MainActivity.java
+++ app/src/main/java/com/xm/jetpacklearning/MainActivity.java
@@ -9,12 +9,12 @@ import com.xm.jetpacklearning.databinding.ActivityMainBinding;
 
 /**
  * 视图绑定有啥作用：
- *      不需要findViewById
- *          null安全。不会有id无效所引发的Null指针风险（id存在但不能在当前view中找到）
- *          类型安全。避免类型转换带来的安全问题（ClassCastException）
- *      和数据绑定的关系：
- *          快，易用
- *          但不支持布局变量表达式（@{}）以及不支持双向数据绑定
+ * 不需要findViewById
+ * null安全。不会有id无效所引发的Null指针风险（id存在但不能在当前view中找到）
+ * 类型安全。避免类型转换带来的安全问题（ClassCastException）
+ * 和数据绑定的关系：
+ * 快，易用
+ * 但不支持布局变量表达式（@{}）以及不支持双向数据绑定
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        View root  = activityMainBinding.getRoot();
+        View root = activityMainBinding.getRoot();
         setContentView(root);
 
-        // 获取布局中有id的某个元素
+        // 数据绑定
+        User user = new User("xiaomei", "zhu");
+        activityMainBinding.setUser(user);
+
+        // 布局绑定：获取布局中有id的某个元素
         activityMainBinding.btnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/xm/jetpacklearning/User.java
@@ -0,0 +1,17 @@
+package com.xm.jetpacklearning;
+
+/**
+ * @author zhuxiaomei
+ * email:  zhuxiaomei.meleny@bytedance.com
+ * date:   2020/7/3
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
