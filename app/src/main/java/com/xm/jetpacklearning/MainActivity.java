7226a1a7f9fe179348e4c26901465c932ab0f175
zhuxiaomei <zhuxiaomei.meleny@bytedance.com>
2020-07-03 14:15:36 +0800

    视图绑定例子，但没啥用

--- app/src/main/java/com/xm/jetpacklearning/MainActivity.java
+++ app/src/main/java/com/xm/jetpacklearning/MainActivity.java
@@ -1,14 +1,23 @@
 package com.xm.jetpacklearning;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.xm.jetpacklearning.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View root  = activityMainBinding.getRoot();
+        setContentView(root);
     }
 }
